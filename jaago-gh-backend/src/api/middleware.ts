49e8ab348a7b6b34ce2e284b9b58418542729773
chavocito <francbota94@gmail.com>
2025-02-13 19:48:47 +0000

    add vendor orders middleware

--- jaago-gh-backend/src/api/middleware.ts
+++ jaago-gh-backend/src/api/middleware.ts
@@ -31,5 +31,12 @@ export default defineMiddlewares({
                 validateAndTransformBody(AdminCreateProduct),
             ],
         },
+        {
+            matcher: "/admin/orders/*",
+            method: "POST",
+            middlewares: [
+                authenticate("vendor", ["session", "bearer"]),
+            ],
+        },
     ],
 })
